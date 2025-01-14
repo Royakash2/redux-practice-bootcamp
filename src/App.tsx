0b917b49096926f872aa01fbf70f40f5ad2d699c
Royakash2 <yahmeakash@gmail.com>
2025-01-15 01:55:27 +0600

    fix all typescript error and add a hook.ts file for manage types

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/features/CounterSlice";
-
+import {
+  decrement,
+  increment,
+  IncrementByValue,
+} from "./redux/features/CounterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const {count} = useSelector((state) => state.counter)
-const dispatch = useDispatch();
-
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
 
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center">
         <div className="flex border border-purple-300 rounded-md bg-slate-300 p-10 gap-4">
-          <button onClick={()=> dispatch(increment())} className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono">
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono"
+          >
             Increment
           </button>
+          <button
+            onClick={() => dispatch(IncrementByValue(5))}
+            className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono"
+          >
+            Increment by value
+          </button>
           <h1 className="text-3xl">{count}</h1>
-          <button onClick={()=> dispatch(decrement())} className="px-3 py-2 rounded-md bg-red-400 font-semibold font-mono">
+          <button
+            onClick={() => dispatch(decrement())}
+            className="px-3 py-2 rounded-md bg-red-400 font-semibold font-mono"
+          >
             Decrement
           </button>
         </div>
--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -1,19 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+type TCounterType = {
+  count: number;
+};
 
-const initialState = { count: 0 };
+const initialState: TCounterType = { count: 0 };
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment: (state, action) => {
+    increment: (state) => {
       state.count = state.count + 1;
     },
-    decrement: (state, action) => {
+    decrement: (state) => {
       state.count = state.count - 1;
+    },
+    IncrementByValue: (state, action: PayloadAction<number>) => {
+      state.count = state.count + action.payload;
     },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, IncrementByValue } = counterSlice.actions;
 export default counterSlice.reducer;
