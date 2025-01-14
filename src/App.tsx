a249c333e2b14a87a6a3cb74fd1b38e905db883a
Royakash2 <yahmeakash@gmail.com>
2025-01-14 08:55:09 +0600

    selector and dispatch used and increment and decrement on ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/CounterSlice";
+
+
 function App() {
+  const {count} = useSelector((state) => state.counter)
+const dispatch = useDispatch();
+
+
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center">
         <div className="flex border border-purple-300 rounded-md bg-slate-300 p-10 gap-4">
-          <button className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono">
+          <button onClick={()=> dispatch(increment())} className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono">
             Increment
           </button>
-          <h1 className="text-3xl">0</h1>
-          <button className="px-3 py-2 rounded-md bg-red-400 font-semibold font-mono">
+          <h1 className="text-3xl">{count}</h1>
+          <button onClick={()=> dispatch(decrement())} className="px-3 py-2 rounded-md bg-red-400 font-semibold font-mono">
             Decrement
           </button>
         </div>
