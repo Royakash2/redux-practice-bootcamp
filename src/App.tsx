07b0a1c08b22430a67f8ed1701ef9ededbca86a6
Royakash2 <yahmeakash@gmail.com>
2025-01-14 08:28:38 +0600

    create src folder install redux tolllkit create store and slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-
-
 function App() {
- 
-
   return (
     <>
-     <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <div className="h-screen w-full flex justify-center items-center">
+        <div className="flex border border-purple-300 rounded-md bg-slate-300 p-10 gap-4">
+          <button className="px-3 py-2 rounded-md bg-green-400 font-semibold font-mono">
+            Increment
+          </button>
+          <h1 className="text-3xl">0</h1>
+          <button className="px-3 py-2 rounded-md bg-red-400 font-semibold font-mono">
+            Decrement
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
